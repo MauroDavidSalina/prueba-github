0ee434d75d9f9d12343e77e11f855d8b5c73c171
Mauro David Salina <maurosalina85@gmail.com>
2020-05-06 21:19:29 -0300

    se dio formato a todos los documentos

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+$(document).ready(function () {
   getContent();
 });
 //funcion para cargar el contenido dinamicamente de indexAjax.html
@@ -6,25 +6,25 @@ function getContent() {
   $.ajax({
     url: "http://www.json-generator.com/api/json/get/bPUkvYyLzC?indent=2",
     success: function (respuesta) {
-        //console.log(Object.keys(respuesta.peliculas[0]));
-        $(".section-central-ind").append(
-          '<section id="s1"><h2>' + respuesta.peliculas[0].genero + '</h2>' +
-          '<div class="articulos-individual">' 
+      //console.log(Object.keys(respuesta.peliculas[0]));
+      $(".section-central-ind").append(
+        '<section id="s1"><h2>' + respuesta.peliculas[0].genero + '</h2>' +
+        '<div class="articulos-individual">'
+      );
+      var articulos = $(".articulos-individual");
+      $.each(respuesta.peliculas, function (index, elemento) {
+        articulos.append(
+          '<article>' +
+          '<img src="' + elemento.poster + '" title="' + elemento.titulo + '">' +
+          '<p>' + elemento.sinopsis + '</p></article></div>'
         );
-        var articulos = $(".articulos-individual");
-        $.each(respuesta.peliculas, function(index,elemento) {
-          articulos.append(
-            '<article>' +
-            '<img src="'+ elemento.poster +'" title="' + elemento.titulo + '">' +
-            '<p>' + elemento.sinopsis + '</p></article></div>'
-          );
-        });
-        //$(".section-central-ind img").css({'width':'200px','height':'200px'});
-        console.log(respuesta);
-      },
-    error: function() {
-        alert("No se ha podido obtener la información");
-        console.log("No se ha podido obtener la información");
+      });
+      //$(".section-central-ind img").css({'width':'200px','height':'200px'});
+      console.log(respuesta);
+    },
+    error: function () {
+      alert("No se ha podido obtener la información");
+      console.log("No se ha podido obtener la información");
     }
   });
 }
--- js/script.js
+++ js/script.js
@@ -1,54 +1,54 @@
-var textAlertVacio="";
-var textAlertFormat="";
+var textAlertVacio = "";
+var textAlertFormat = "";
 
-function nombrevacio(){
-    if($('#nombre').val() == ""){
-        textAlertVacio+= "El nombre no puede estar vacio\n";
+function nombrevacio() {
+    if ($('#nombre').val() == "") {
+        textAlertVacio += "El nombre no puede estar vacio\n";
         //alert("El nombre no puede estar vacio");
         return false;
     }
     return true;
 }
 
-function apellidoVacio(){
-    if ($('#apellido').val() == ""){
-        textAlertVacio+= "El apellido no puede estar vacio\n";
+function apellidoVacio() {
+    if ($('#apellido').val() == "") {
+        textAlertVacio += "El apellido no puede estar vacio\n";
         //alert("El apellido no puede estar vacio");
         return false;
     }
     return true;
 }
 
-function nacimientoVacio(){
-    if ($('#nac').val() == ""){
-        textAlertVacio+= "La fecha de nacimiento no puede estar vacio\n";
+function nacimientoVacio() {
+    if ($('#nac').val() == "") {
+        textAlertVacio += "La fecha de nacimiento no puede estar vacio\n";
         //alert("El nacimiento no puede estar vacio");
         return false;
     }
     return true;
 }
 
-function sexoVacio(){
+function sexoVacio() {
     if (!document.querySelector('input[name="sexo"]:checked')) {
-        textAlertVacio+= "Debe seleccionar el sexo\n";
+        textAlertVacio += "Debe seleccionar el sexo\n";
         //alert("El sexo no puede estar vacio");
         return false;
     }
     return true;
 }
 
-function valoracionVacia(){
-    if($('#valoracion').val() == ""){
-        textAlertVacio+= "La valoracion no puede estar vacio\n";
+function valoracionVacia() {
+    if ($('#valoracion').val() == "") {
+        textAlertVacio += "La valoracion no puede estar vacio\n";
         //alert("La valoracion no puede estar vacio");
         return false;
     }
     return true;
 }
 
-function emailVacio(){
-    if($('#email').val() == "" ){
-        textAlertVacio+= "El email no puede estar vacio\n";
+function emailVacio() {
+    if ($('#email').val() == "") {
+        textAlertVacio += "El email no puede estar vacio\n";
         //alert("El email no puede estar vacio");
         return false;
     }
@@ -56,70 +56,70 @@ function emailVacio(){
 }
 
 function validarNombre() {
-    var reg =/^[a-zA-Z]+$/;
+    var reg = /^[a-zA-Z]+$/;
     if ($('#nombre').val().match(reg)) {
         console.log("Nombre validado");
         return true;
     } else {
-        textAlertFormat+= "Este nombre es incorrecto\n";
+        textAlertFormat += "Este nombre es incorrecto\n";
         //alert("Este nombre es incorrecto");
         return false;
     }
 }
 
 function validarApellido() {
-    var reg =/^[a-zA-Z]+$/; 
+    var reg = /^[a-zA-Z]+$/;
     if ($('#apellido').val().match(reg)) {
         console.log("Apellido validado");
         return true;
     } else {
-        textAlertFormat+= "Este apellido es incorrecto\n";
+        textAlertFormat += "Este apellido es incorrecto\n";
         //alert("Este apellido es incorrecto");
         return false;
     }
 }
 
-function validarCorreo(){
-    var reg=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if ($('#email').val().match(reg)){
+function validarCorreo() {
+    var reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if ($('#email').val().match(reg)) {
         console.log("Email validado");
         return true;
-    }else{
-        textAlertFormat+= "Correo incorrecto\n";
+    } else {
+        textAlertFormat += "Correo incorrecto\n";
         //alert("Correo incorrecto");
         return false;
     }
 }
 
-function validadordeVacios(){
-     var nac=nacimientoVacio();
-     var nom=nombrevacio();
-     var ape=apellidoVacio();
-     var val=valoracionVacia();
-     var sexo=sexoVacio();
-     var email=emailVacio();
+function validadordeVacios() {
+    var nac = nacimientoVacio();
+    var nom = nombrevacio();
+    var ape = apellidoVacio();
+    var val = valoracionVacia();
+    var sexo = sexoVacio();
+    var email = emailVacio();
 
-     if(nac==true && nom==true && ape==true && val==true && sexo==true && email==true){
+    if (nac == true && nom == true && ape == true && val == true && sexo == true && email == true) {
         return true;
-     }else{
+    } else {
         alert(textAlertVacio);//alert con todas las validaciones que fallaron
         return false;
-     }
+    }
 }
 
-function validadordeFormatos(){
-    var valNom=validarNombre();
-    var valApe=validarApellido();
-    var valCor=validarCorreo();
-    if (valApe==true && valCor==true && valNom==true){
+function validadordeFormatos() {
+    var valNom = validarNombre();
+    var valApe = validarApellido();
+    var valCor = validarCorreo();
+    if (valApe == true && valCor == true && valNom == true) {
         return true;
-    }else{
+    } else {
         alert(textAlertFormat);//alert con los formtos que fallaron
         return false;
     }
 }
 
-function mostrarDatos(){
+function mostrarDatos() {
     alert(
         "Nombre:\t" + $('#nombre').val() +
         "\nApellido:\t" + $('#apellido').val() +
@@ -128,19 +128,19 @@ function mostrarDatos(){
         "\nValoracion:\t" + $('#valoracion').val() +
         "\nCorreo:\t" + $('#email').val()
     );
-    
+
 }
 
-function cancelar(){
-    var decision= confirm("¿Desea volver al menu principal?");
-    if (decision==true){
-        window.location.href="./Index.html";
-    }else{
+function cancelar() {
+    var decision = confirm("¿Desea volver al menu principal?");
+    if (decision == true) {
+        window.location.href = "./Index.html";
+    } else {
         return false;
     }
 }
 
-function restablecer(){
+function restablecer() {
     $('#form-general')[0].reset();
     //document.getElementById("form-general").reset();
 }
@@ -148,34 +148,34 @@ function restablecer(){
 //Funcion principal
 $(document).ready(function () {
     //click en boton enviar
-    $("#enviar").click(function(){
+    $("#enviar").click(function () {
         console.log("ingresando a la funcion de validacion de valores nulos");
-        var vacios=validadordeVacios();
+        var vacios = validadordeVacios();
         console.log("Validación de nombre y apellido");
         console.log("Validando correo...");
-        var formatos=validadordeFormatos();
-        if(vacios==true && formatos==true){
+        var formatos = validadordeFormatos();
+        if (vacios == true && formatos == true) {
             mostrarDatos();
         }
     });
     //click en boton cancelar
-    $("#cancelar").click(function(){
+    $("#cancelar").click(function () {
         cancelar();
     });
     //click en boton restablecer
-    $("#restablecer").click(function(){
+    $("#restablecer").click(function () {
         restablecer();
     });
     //carrito (click en un articulo)
     var carrito = 0;
-    $('#sec-cen article').click(function(evento){
+    $('#sec-cen article').click(function (evento) {
         $(this).toggleClass('articleclick');
-        if ($(evento.currentTarget).hasClass('articleclick')){
+        if ($(evento.currentTarget).hasClass('articleclick')) {
             carrito += 250;
-            $('#costo').html("Carrito: $"+ carrito);
-        }else{
+            $('#costo').html("Carrito: $" + carrito);
+        } else {
             carrito -= 250;
-            $('#costo').html("Carrito: $"+ carrito);
+            $('#costo').html("Carrito: $" + carrito);
         }
     });
 
